667b0bf11ea430c69d760ebf11b71f743cba3e96
Jesse Jaara <jesse.jaara@vincit.fi>
2026-02-19 00:03:23 +0200

    Rework Java Actuator client
    * Add license and original source notice.
    * Add compilation instructions.
    * Recompile for Java 21, so that this can be used in Tiamat.
    * Rename from Heal__ht__Check to Heal__th__Check.

---
+++ docker/HealthCheck.java
@@ -0,0 +1,43 @@
+// Source: https://www.naiyerasif.com/post/2021/03/01/java-based-health-check-for-docker/
+// Original code license: CC BY-SA 4.0 | https://www.naiyerasif.com/about/#frequently-asked-questions
+
+/**
+ * How to compile:
+ * javac HealthCheck.java
+ * jar --create --file HealthCheck.jar --main-class HealthCheck HealthCheck.class
+ * rm HealthCheck.class
+ *
+ * Remember to compile and/or target for the proper Java version.
+ * Currently Tiamat requires 21, while others could support 25.
+ * So 21 is used as the baseline support level.
+ */
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
+
+public class HealthCheck {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final var client = HttpClient.newHttpClient();
+        final var request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8080/actuator/health"))
+                .header("accept", "application/json")
+                .build();
+
+        final var response = client.send(request, BodyHandlers.ofString());
+        final var body = response.body();
+
+        System.out.print("Status: " + response.statusCode());
+        System.out.print(" | body: " + body);
+        System.out.println();
+
+        if (response.statusCode() != 200 || !body.contains("UP")) {
+            System.err.println("Service is not UP!");
+            System.exit(1);
+        } else {
+            System.exit(0);
+        }
+    }
+}
